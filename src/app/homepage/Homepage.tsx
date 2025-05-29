cc9e6a5b893d15b71bbb191a5f6a06a91097146a
samer <judosamer555@gmail.com>
2025-05-29 23:56:08 +0300

    feat(create-setup): add cn utility and update styling configuration

--- src/app/homepage/Homepage.tsx
+++ src/app/homepage/Homepage.tsx
@@ -2,5 +2,10 @@ import { useTranslations } from "use-intl";
 
 export default function Homepage() {
   const t = useTranslations();
-  return <div>{t("homepage")}</div>;
+  return (
+    <div>
+      <h1 className=" text-9xl bg-custom-orange-900">homepage</h1>
+      {t("homepage")}
+    </div>
+  );
 }
