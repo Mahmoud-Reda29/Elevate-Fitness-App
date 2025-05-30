e845aef511fba185838e601bc36a32746e8cda5e
samer <judosamer555@gmail.com>
2025-05-30 08:39:25 +0300

    feat/create-setup : add comments and add jsDocs to how add custom color, change title and fav icon

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 export default function AppLayout() {
   return (
     <div>
+      {/* Outlet children */}
       <Outlet />
     </div>
   );
--- src/app/homepage/Homepage.tsx
+++ src/app/homepage/Homepage.tsx
@@ -1,7 +1,9 @@
 import { useTranslations } from "use-intl";
 
 export default function Homepage() {
+  // Translation
   const t = useTranslations();
+
   return (
     <div>
       <h1 className="bg-custom-orange-900 text-9xl">homepage (hodaaaa elevate)</h1>
