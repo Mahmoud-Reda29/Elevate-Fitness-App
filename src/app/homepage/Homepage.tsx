85180d9eef8213fddbfcf123f238c0864161d076
khalid <khalid.mossad123456@gail.com>
2025-07-03 15:15:19 +0300

    remove old code

--- src/app/homepage/Homepage.tsx
+++ src/app/homepage/Homepage.tsx
@@ -1,15 +1,9 @@
-import { useTranslations } from "use-intl";
 import Workouts from "./workouts/Workout";
 import BestWay from "./bestway/BestWay";
 
 export default function Homepage() {
-  // Translation
-  const t = useTranslations();
-
   return (
     <div>
-      <h1 className="bg-custom-orange-900 text-9xl">homepage (hodaaaa elevate)</h1>
-      {t("homepage")}
       <Workouts />
       <BestWay />
     </div>
